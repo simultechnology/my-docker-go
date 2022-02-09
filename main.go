e4b262ee491746553881f7fbefac34a6a0958f7c
t_ishikawa <tissi0708@gmail.com>
2022-02-10 00:03:57 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		nodeIp := os.Getenv("NODE_IP")
+		nodeName := os.Getenv("NODE_NAME")
+		return c.HTML(http.StatusOK, fmt.Sprintf("NODE_IP: %s<br>NODE_NAME: %s", nodeIp, nodeName))
+	})
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+
+}
